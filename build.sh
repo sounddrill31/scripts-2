e5733ba1c4ab5d43668679bf664defe159f533bc
xyz-sundram <sundrraam@gmail.com>
2024-03-22 11:03:32 +0530

    Leaf OS
    also make build and upload script executable

--- build.sh
+++ build.sh
@@ -2,20 +2,17 @@
 
 # init and sync
 rm -rf .repo/local_manifests
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-git clone https://github.com/xyz-sundram/local_manifest.git -b rising .repo/local_manifests
+repo init -u https://github.com/LeafOS-Project/android.git -b leaf-3.1
+git clone https://github.com/xyz-sundram/local_manifest.git -b leaf .repo/local_manifests
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 
-rm -rf hardware/qcom-caf/common/
-git clone https://github.com/xyz-sundram/android_hardware_qcom-caf_common.git hardware/qcom-caf/common/
-
-cd system/libhidl 
-git fetch https://github.com/Evolution-X/system_libhidl f93f60fa2b93b6ff1699f1cfc6e3192fcbe7f36e
-git cherry-pick f93f60fa2b93b6ff1699f1cfc6e3192fcbe7f36e
-cd ../..
+# source patches
+cd hardware/qcom-caf/common/
+curl https://github.com/xyz-sundram/android_hardware_qcom-caf_common/commit/99fb248c6f67772c44686f5b117c911ffd70fd6f.patch | git am
+cd ../../..
 
 # build rom
 source build/envsetup.sh
 export SELINUX_IGNORE_NEVERALLOWS=true
-lunch rising_tulip-userdebug
-mka bacon
+lunch tulip-userdebug
+m leaf
